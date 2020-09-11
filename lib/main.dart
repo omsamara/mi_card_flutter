23818ab4df44ab5b3204940a150b1d7cd0189141
Omar Samara <omarsamara6@gmail.com>
2020-09-10 20:51:39 -0400

    Safe area, center text widgets

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Container(
+            color: Colors.white,
+            child: Center(child: Text("Hello")),
+            width: 100,
+            height: 100,
+            margin: EdgeInsets.only(left: 50, top: 50),
+          ),
+        ),
       ),
     );
   }
